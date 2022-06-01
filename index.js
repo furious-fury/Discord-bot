3c5d3bf97aab090bdc3525f1cfa06741090f4f35
Egbaoghene Akon <65835823+furious-fury@users.noreply.github.com>
2022-06-01 23:52:27 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const axios = require('axios').default;
+
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.BOT_MEME);
+
+client.on('message', async msg => {
+  async function getMeme(){
+    const res = await axios.get('https://memeapi.pythonanywhere.com/');
+    return res.data.memes[0].url;
+  }
+    switch (msg.content) {
+      //our meme command below
+      case "!meme":
+        msg.channel.send("Here's your meme!"); //Replies to user command
+        const img = await getMeme(); //fetches an URL from the API
+        msg.channel.send(img); //send the image URL
+        break;
+     }
+  })
+
