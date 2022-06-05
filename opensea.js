9031daf534d420076ed98110d5e81ee06e6a2317
Egbaoghene Akon <65835823+furious-fury@users.noreply.github.com>
2022-06-05 17:34:47 +0100

    handled errors

--- opensea.js
+++ opensea.js
@@ -23,12 +23,13 @@ client.on('messageCreate', async messageCreate => {
             function kFormatter(num) {
                 return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'k' : Math.sign(num)*Math.abs(num)
             }
-                const openseaEmbed = new MessageEmbed()
+
+                const successEmbed = new MessageEmbed()
                     .setColor('#218500')
                     .setTitle(`${res.data.collection.name}`)
                     .setURL(`https://opensea.io/collection/${res.data.collection.slug}`)
                     .setDescription(`${res.data.collection.description}`)
-                    .setThumbnail(`${res.data.collection.featured_image_url}`)
+                    .setThumbnail(`${res.data.collection.image_url}`)
                     .addFields(
                         { name: 'FLOOR PRICE', value: `Ξ${res.data.collection.stats.floor_price.toFixed(3)}` },
                         { name: 'VOLUME', value: `Ξ${kFormatter(res.data.collection.stats.total_volume.toFixed(3))}` },
@@ -37,9 +38,18 @@ client.on('messageCreate', async messageCreate => {
                     )
                     .setTimestamp()
                     .setFooter({ text: 'All data gotten from Opensea', iconURL: 'https://storage.googleapis.com/opensea-static/Logomark/Logomark-Blue.png' });
-                    messageCreate.reply({ embeds: [openseaEmbed] });
+                    messageCreate.reply({ embeds: [successEmbed] });
         })
-        .catch(err => console.error(err.success));
+        .catch(err => {
+            const errorEmbed = new MessageEmbed()
+            .setColor('#D20e12')
+            .setTitle('ERROR !')
+            .setDescription("The bot couldn't find the requested collectiond because the requested name may not be the same on opensea. Please crosscheck to see if the name matches the one on the opensea URL")
+            .setImage('https://fieryper.sirv.com/opensea.jpg')
+            .setTimestamp()
+            .setFooter({ text: 'All data gotten from Opensea', iconURL: 'https://storage.googleapis.com/opensea-static/Logomark/Logomark-Blue.png' });
+            messageCreate.reply({ embeds: [errorEmbed] });
+        });
             }
     });
 
